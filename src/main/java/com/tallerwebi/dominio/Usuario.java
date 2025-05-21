f33775aab076af46d2dbdc07e228485afbf0dc84
MartinMeza22 <martinmeza.t22@gmail.com>
2025-05-21 20:56:17 -0300

    Controller / View

---
+++ src/main/java/com/tallerwebi/dominio/Usuario.java
@@ -0,0 +1,112 @@
+package com.tallerwebi.dominio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String rol;
+    private Long carreraID;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String situacionLaboral;
+    private Integer disponibilidadHoraria;
+
+    private Boolean activo = false;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public Boolean getActivo() {
+        return activo;
+    }
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public Long getCarreraID() {
+        return carreraID;
+    }
+
+    public void setCarreraID(Long carreraID) {
+        this.carreraID = carreraID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getSituacionLaboral() {
+        return situacionLaboral;
+    }
+
+    public void setSituacionLaboral(String situacionLaboral) {
+        this.situacionLaboral = situacionLaboral;
+    }
+
+    public Integer getDisponibilidadHoraria() {
+        return disponibilidadHoraria;
+    }
+
+    public void setDisponibilidadHoraria(Integer disponibilidadHoraria) {
+        this.disponibilidadHoraria = disponibilidadHoraria;
+    }
+
+    public boolean activo() {
+        return activo;
+    }
+
+    public void activar() {
+        activo = true;
+    }
+}
