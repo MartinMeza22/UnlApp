00f19b0dd170018068c788456e87ac1d991eea61
Franco Nadal <fnadal01@gmail.com>
2025-06-28 11:52:24 -0300

    implementacion del Reiniciardb y preparacion del vistaresumen itenelgente2e2e
    preparo los metodos before , after , before each y aftereach para implementar y preparar los tests

--- src/test/java/com/tallerwebi/punta_a_punta/VistaResumenInteligenteE2E.java
+++ src/test/java/com/tallerwebi/punta_a_punta/VistaResumenInteligenteE2E.java
@@ -1,4 +1,43 @@
 package com.tallerwebi.punta_a_punta;
 
+import com.microsoft.playwright.*;
+import com.tallerwebi.punta_a_punta.vistas.VistaHome;
+import com.tallerwebi.punta_a_punta.vistas.VistaLogin;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.Path;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class VistaResumenInteligenteE2E {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    VistaLogin vistaLogin;
+    Page page;
+
+    @BeforeAll
+    static void abrirNavegador() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+    }
+
+    @AfterAll
+    static void cerrarNavegador() {
+        playwright.close();
+    }
+    @BeforeEach
+    void crearContextoYPagina() {
+        ReiniciarDB.limpiarBaseDeDatos();
+
+        context = browser.newContext(new Browser.NewContextOptions().setAcceptDownloads(true)); // clave para capturar descargas
+        page = context.newPage();
+        vistaLogin = new VistaLogin(page);
+    }
+
+    @AfterEach
+    void cerrarContexto() {
+        context.close();
+    }
 }
