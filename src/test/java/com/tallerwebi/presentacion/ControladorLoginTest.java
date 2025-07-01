360452adbecd735e8761e3c9d4cbb9f916ebe1f6
MartinMeza22 <martinmeza.t22@gmail.com>
2025-07-01 19:08:07 -0300

    Adición de test - LoginController

--- src/test/java/com/tallerwebi/presentacion/ControladorLoginTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorLoginTest.java
@@ -12,6 +12,7 @@ import com.tallerwebi.servicioInterfaz.ServicioCarrera;
 import com.tallerwebi.servicioInterfaz.ServicioUsuario;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.InstanceOf;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.web.servlet.ModelAndView;
@@ -20,15 +21,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.equalTo;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 
 
 public class ControladorLoginTest {
@@ -116,9 +120,9 @@ public class ControladorLoginTest {
         //Login - Cualquier vista
     @Test
     public void debeRetornarLaPaginaLoginCuandoSeNavegaALaRaiz() throws Exception {
-        ModelAndView mav = controladorLogin.irALogin();
+        ModelAndView mav = controladorLogin.inicio();
         assert mav != null;
-        assertThat(mav.getViewName(), equalToIgnoringCase("login"));
+        assertThat(mav.getViewName(), equalToIgnoringCase("redirect:/login"));
     }
         //Login
     @Test
@@ -131,7 +135,6 @@ public class ControladorLoginTest {
         assertThat(mav.getViewName(), equalToIgnoringCase("login"));
         assertThat(mav.getModel().get("datosLogin").toString(), containsString("com.tallerwebi.dominio.DatosLogin"));
     }
-
 
     //Register
     @Test
@@ -325,7 +328,95 @@ public class ControladorLoginTest {
         assertThat(mav.getViewName(), equalTo("nuevo-usuario"));
         assertThat(mav.getModel().get("error"), is("La disponibilidad horaria es obligatoria"));
     }
+
+    //Revisar
+    @Test
+    public void registrarseConUnMailYaRegistradoDevuelveError() throws UsuarioExistente {
+        // Nuevo usuario con mismo mail
+        Usuario usuarioNuevo = new Usuario();
+        usuarioNuevo.setEmail("martin@gmail.com");
+        usuarioNuevo.setPassword("1234");
+        usuarioNuevo.setNombre("Marttin");
+        usuarioNuevo.setApellido("Mezza");
+        usuarioNuevo.setCarreraID(1L);
+        usuarioNuevo.setSituacionLaboral("Desempleado");
+        usuarioNuevo.setDisponibilidadHoraria(20);
+
+        doThrow(new UsuarioExistente()).when(repositorioLoginMock).registrar(any(Usuario.class));
+
+        ModelAndView mv = controladorLogin.registrarme(usuarioNuevo, requestMock);
+
+        assertThat(mv.getViewName(), is("nuevo-usuario")); // ✅ chequear la vista también
+        assertThat(mv.getModel().get("error"), is("El usuario ya existe"));
+    }
+
+
+//    @Test
+//    public void elCodigoDeVerificacionSeEnviaCorrectamente() throws UsuarioExistente {
+//        Usuario usuario = new Usuario();
+//        usuario.setEmail("martin@gmail.com");
+//        usuario.setPassword("1234");
+//        usuario.setNombre("Martín");
+//        usuario.setApellido("Meza");
+//        usuario.setCarreraID(1L);
+//        usuario.setSituacionLaboral("Estudiante");
+//        usuario.setDisponibilidadHoraria(20);
+//
+//        Usuario usuarioPersistido = new Usuario();
+//        usuarioPersistido.setId(1L);
+//
+//        // Simular comportamiento
+//        doNothing().when(repositorioLoginMock).registrar(any());
+//        when(repositorioLoginMock.consultarUsuario("martin@gmail.com", "1234")).thenReturn(usuarioPersistido);
+//        doNothing().when(servicioEmailMock).guardarYEnviarCodigoDeVerificacion(any());
+//
+//        ModelAndView mv = controladorLogin.registrarme(usuario, requestMock);
+//
+//        // Verificaciones
+//        verify(repositorioLoginMock).registrar(usuario);
+//        verify(repositorioLoginMock).consultarUsuario("martin@gmail.com", "1234");
+//        verify(servicioEmailMock).guardarYEnviarCodigoDeVerificacion(usuario);
+//
+//        assertThat(mv.getViewName(), is("verificar-token"));
+//    }
+//FALTA VERIFICAR TODA LA RUTA DEL TOKEN
+
+    @Test
+    public void cuandoSeCreaElUSuarioElMismoTieneAccesoATodasLasMateriasDeLaCarrera(){
+        Usuario usuario = new Usuario();
+        List<Carrera> carreras = List.of(new Carrera("Tuko"),
+        new Carrera("Tukin"));
+
+        when(servicioCarreraMock.obtenerTodasLasCarreras()).thenReturn(carreras);
+
+        ModelAndView mav = controladorLogin.nuevoUsuario();
+
+        assertThat(mav.getViewName(), is("nuevo-usuario")); //Verifico el nombre de la vista
+        assertThat(mav.getModel().get("usuario"), instanceOf(Usuario.class)); //Verifico si el usuario del modelo es un objeto Usuario
+        assertThat(mav.getModel().get("carreras"), is(carreras)); //Verifico si el modelo de carreras tiene las carreras que ingresé
+        verify(servicioCarreraMock).obtenerTodasLasCarreras(); //Verifico que se haya llamado al servicio
+    }
+
+    @Test
+    public void cuandoSeSeleccionaCerrarSesionLaSesionSeCierra(){
+        Usuario usuario = new Usuario();
+        usuario.setId(4L);
+        usuario.setApellido("Meza");
+        usuario.setNombre("Martin");
+        usuario.setEmail("martin@gmail.com");
+        usuario.setPassword("1234");
+        usuario.setCarreraID(1L);
+        usuario.setSituacionLaboral("Desempleado");
+        usuario.setDisponibilidadHoraria(20);
+        when(mockHttpServletRequestMock.getSession(false)).thenReturn(sessionMock);
+
+        ModelAndView mav = controladorLogin.logout(mockHttpServletRequestMock);
+
+        assertThat(mav.getViewName(), is("redirect:/login"));
+    }
+
 }
+
 
 
 //Preparás los datos y mocks (Setup).
