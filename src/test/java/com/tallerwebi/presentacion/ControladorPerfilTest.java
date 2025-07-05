0ce7c08dd6c6c8d758ea38ff5327100601bb7168
Franco Nadal <fnadal01@gmail.com>
2025-07-05 13:50:38 -0300

    queSegenereElCvCorrectamente TEST

--- src/test/java/com/tallerwebi/presentacion/ControladorPerfilTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorPerfilTest.java
@@ -1,18 +1,20 @@
 package com.tallerwebi.presentacion;
 
-import com.tallerwebi.dominio.servicios.ServicioUsuarioMateria;
-import com.tallerwebi.repositorioInterfaz.RepositorioUsuario;
-import com.tallerwebi.dominio.*;
+import com.tallerwebi.dominio.Usuario;
+import com.tallerwebi.dominio.UsuarioMateria;
 import com.tallerwebi.dominio.excepcion.UsuarioNoEncontrado;
+import com.tallerwebi.dominio.servicios.ServicioUsuarioMateria;
 import com.tallerwebi.servicioInterfaz.ServicioUsuario;
+import com.tallerwebi.serviciosImplementacion.ServicioCvImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -22,6 +24,7 @@ public class ControladorPerfilTest {
 
     private ServicioUsuario servicioUsuario;
     private ServicioUsuarioMateria servicioUsuarioMateria;
+    private ServicioCvImpl servicioCv;
     private ControladorPerfil controladorPerfil;
 
     private HttpServletRequest request;
@@ -33,7 +36,8 @@ public class ControladorPerfilTest {
     public void setUp() {
         servicioUsuario = mock(ServicioUsuario.class);
         servicioUsuarioMateria = mock(ServicioUsuarioMateria.class);
-        controladorPerfil = new ControladorPerfil(servicioUsuario, servicioUsuarioMateria);
+        servicioCv = mock(ServicioCvImpl.class);
+        controladorPerfil = new ControladorPerfil(servicioUsuario, servicioUsuarioMateria, servicioCv);
 
         request = mock(HttpServletRequest.class);
         session = mock(HttpSession.class);
@@ -72,8 +76,6 @@ public class ControladorPerfilTest {
     @Test
     public void queElControladorLlameAlServicioParaActualizarElPerfil() throws UsuarioNoEncontrado {
         when(session.getAttribute("ID")).thenReturn(1L);
-        when(servicioUsuario.obtenerUsuario(1L)).thenReturn(usuario);
-
 
         ModelAndView mav = controladorPerfil.actualizarPerfil(
                 "Franco", "Nadal", "nuevo@email.com", "", request
@@ -81,14 +83,11 @@ public class ControladorPerfilTest {
 
         assertThat(mav.getViewName(), is("redirect:/perfil"));
         verify(servicioUsuario).actualizarPerfil(1L, "Franco", "Nadal", "nuevo@email.com", "");
-
-
     }
 
     @Test
     public void queElControladorDelegueLaActualizacionDePasswordAlServicio() throws UsuarioNoEncontrado {
         when(session.getAttribute("ID")).thenReturn(1L);
-        when(servicioUsuario.obtenerUsuario(1L)).thenReturn(usuario);
 
         ModelAndView mav = controladorPerfil.actualizarPerfil(
                 "Franco", "Nadal", "franco@test.com", "nuevaPassword123", request
@@ -97,7 +96,6 @@ public class ControladorPerfilTest {
         assertThat(mav.getViewName(), is("redirect:/perfil"));
         verify(servicioUsuario).actualizarPerfil(1L, "Franco", "Nadal", "franco@test.com", "nuevaPassword123");
     }
-
 
     @Test
     public void queSePuedaEliminarCuentaSiHaySesionActiva() throws UsuarioNoEncontrado {
@@ -118,10 +116,10 @@ public class ControladorPerfilTest {
 
         assertThat(mav.getViewName(), is("redirect:/login"));
     }
+
     @Test
     public void queElControladorDelegueLaActualizacionDelNombreAlServicio() throws UsuarioNoEncontrado {
         when(session.getAttribute("ID")).thenReturn(1L);
-        when(servicioUsuario.obtenerUsuario(1L)).thenReturn(usuario);
 
         ModelAndView mav = controladorPerfil.actualizarPerfil(
                 "NuevoNombre", "Nadal", "franco@test.com", null, request
@@ -131,11 +129,9 @@ public class ControladorPerfilTest {
         verify(servicioUsuario).actualizarPerfil(1L, "NuevoNombre", "Nadal", "franco@test.com", null);
     }
 
-
     @Test
     public void queElControladorDelegueLaActualizacionDelApellidoAlServicio() throws UsuarioNoEncontrado {
         when(session.getAttribute("ID")).thenReturn(1L);
-        when(servicioUsuario.obtenerUsuario(1L)).thenReturn(usuario);
 
         ModelAndView mav = controladorPerfil.actualizarPerfil(
                 "Franco", "NuevoApellido", "franco@test.com", null, request
@@ -145,5 +141,27 @@ public class ControladorPerfilTest {
         verify(servicioUsuario).actualizarPerfil(1L, "Franco", "NuevoApellido", "franco@test.com", null);
     }
 
+
+    // NUEVO TEST para el endpoint /perfil/generar-cv
+    @Test
+    public void queSeGenereElCVCorrectamente() throws Exception {
+        when(session.getAttribute("ID")).thenReturn(1L);
+
+        UsuarioMateria materiaAprobada = mock(UsuarioMateria.class);
+        when(materiaAprobada.estaAprobada()).thenReturn(true);
+
+        List<UsuarioMateria> materias = Collections.singletonList(materiaAprobada);
+        when(servicioUsuarioMateria.mostrarMateriasDeUsuario(null, 1L)).thenReturn(materias);
+        when(servicioCv.generarYGuardarCV(1L, materias)).thenReturn("Texto del CV generado");
+
+        ResponseEntity<String> response = controladorPerfil.generarCV(session);
+
+        assertThat(response.getStatusCodeValue(), is(200));
+        assertThat(response.getBody(), is("Texto del CV generado"));
+        verify(servicioCv).generarYGuardarCV(1L, materias);
+    }
+
+
+   
 
 }
