ea611ea3e44abe9c415135ca4b570b3d45663c03
MartinMeza22 <martinmeza.t22@gmail.com>
2025-07-10 15:52:23 -0300

    TDD E2E

--- src/test/java/com/tallerwebi/punta_a_punta/VistaProgresoYModalMateriaE2E.java
+++ src/test/java/com/tallerwebi/punta_a_punta/VistaProgresoYModalMateriaE2E.java
@@ -28,11 +28,11 @@ public class VistaProgresoYModalMateriaE2E {
     static void cerrarNavegador() {
         playwright.close();
     }
+
     @BeforeEach
     void crearContextoYPagina() {
-        // ReiniciarDB.limpiarBaseDeDatos();
-
-        context = browser.newContext(new Browser.NewContextOptions().setAcceptDownloads(true)); // clave para capturar descargas
+        ReiniciarDB.limpiarBaseDeDatos(); // <<< ROLLBACK ACTIVADO ACÁ
+        context = browser.newContext(new Browser.NewContextOptions().setAcceptDownloads(true));
         page = context.newPage();
         vistaLogin = new VistaLogin(page);
     }
@@ -41,50 +41,47 @@ public class VistaProgresoYModalMateriaE2E {
     void cerrarContexto() {
         context.close();
     }
+
     private void realizarLoginComoTestUser() {
         vistaLogin.escribirEMAIL("user@gmail.com");
         vistaLogin.escribirClave("12345678");
         vistaLogin.darClickEnIniciarSesion();
-        // Esperar a que aparezca el botón que solo está en el home
         page.waitForSelector("text=Ver progreso académico");
     }
 
     @Test
-    public void deberiaPermitirCursarYPonerNotaAUnaMateria(){
+    public void deberiaPermitirCursarYPonerNotaAUnaMateria() {
         realizarLoginComoTestUser();
         page.click("text=Ver progreso académico");
         page.waitForURL("**/progreso");
 
-        String materiaACursar = "Programación Básica II"; // ESTA MATERIA TIENE QUE TENER EL BOTON DE "CURSAR" PARA QUE FUNCIONE EL TEST
+        String materiaACursar = "Programación Básica II";
         String notaFinal = "8";
         String dificultad = "2";
 
         VistaProgreso vistaProgreso = new VistaProgreso(page);
-
         vistaProgreso.cursarMateria(materiaACursar);
 
         page.waitForURL("**/progreso");
-        String estadoDespuesDeCursar = vistaProgreso.obtenerEstadoMateria(materiaACursar).trim();;
+        String estadoDespuesDeCursar = vistaProgreso.obtenerEstadoMateria(materiaACursar).trim();
         assertThat("La materia debería estar en estado CURSANDO", estadoDespuesDeCursar, is("CURSANDO"));
 
         vistaProgreso.abrirModalDeEdicion(materiaACursar);
-
         page.waitForSelector("#materiaEditModal", new Page.WaitForSelectorOptions().setState(WaitForSelectorState.VISIBLE));
 
         vistaProgreso.editarNotaYDificultadEnModal(notaFinal, dificultad);
-
         vistaProgreso.guardarCambiosModal();
 
         page.waitForURL("**/progreso");
         String estadoFinal = vistaProgreso.obtenerEstadoMateria(materiaACursar).trim();
         String notaObtenida = vistaProgreso.obtenerNotaDeMateria(materiaACursar).trim();
 
-        assertThat("La materia debería esta APROBADA", estadoFinal, is("APROBADA"));
+        assertThat("La materia debería estar APROBADA", estadoFinal, is("APROBADA"));
         assertThat("La nota debería ser la que se guardó", notaObtenida, is(notaFinal));
     }
 
     @Test
-    public void deberiaPermitirModificarUnaMateriaPonerlaDesaprobadaConDificultadDificilYGuardarla(){
+    public void deberiaPermitirModificarUnaMateriaPonerlaDesaprobadaConDificultadDificilYGuardarla() {
         realizarLoginComoTestUser();
         page.click("text=Ver progreso académico");
         page.waitForURL("**/progreso");
@@ -96,23 +93,21 @@ public class VistaProgresoYModalMateriaE2E {
         VistaProgreso vistaProgreso = new VistaProgreso(page);
 
         vistaProgreso.abrirModalDeEdicion(materiaAModificar);
-
         page.waitForSelector("#materiaEditModal", new Page.WaitForSelectorOptions().setState(WaitForSelectorState.VISIBLE));
 
         vistaProgreso.editarNotaYDificultadEnModal(nuevaNota, nuevaDificultad);
-
         vistaProgreso.guardarCambiosModal();
 
         page.waitForURL("**/progreso");
         String estadoFinal = vistaProgreso.obtenerEstadoMateria(materiaAModificar).trim();
         String notaObtenida = vistaProgreso.obtenerNotaDeMateria(materiaAModificar).trim();
 
-        assertThat("La materia debería cambiar su estado a DESAPROBADO", estadoFinal, is("DESAPROBADA"));
-        assertThat("La nota debería ser 2", notaObtenida, is(nuevaNota));
+        assertThat("La materia debería estar DESAPROBADA", estadoFinal, is("DESAPROBADA"));
+        assertThat("La nota debería ser la modificada", notaObtenida, is(nuevaNota));
     }
 
     @Test
-    public void queCuandoElUsuarioPongaCursarUnaMateriaPeroSeArreptientaYVuelvaDejarlaMateriaYSuEstadoSeaPendiente(){
+    public void queCuandoElUsuarioPongaCursarUnaMateriaPeroSeArrepientaYVuelvaDejarlaMateriaYSuEstadoSeaPendiente() {
         realizarLoginComoTestUser();
         page.click("text=Ver progreso académico");
         page.waitForURL("**/progreso");
@@ -123,43 +118,37 @@ public class VistaProgresoYModalMateriaE2E {
 
         vistaProgreso.cursarMateria(materiaADejarDeCursar);
         String obtenerEstadoDeLaMateria = vistaProgreso.obtenerEstadoMateria(materiaADejarDeCursar);
-        assertThat("La materia deberia cambia su estado a CURSANDO", obtenerEstadoDeLaMateria, is("CURSANDO"));
+        assertThat("La materia debería estar CURSANDO", obtenerEstadoDeLaMateria, is("CURSANDO"));
 
         vistaProgreso.dejarMateria(materiaADejarDeCursar);
-
         page.waitForURL("**/progreso");
+
         String estadoFinal = vistaProgreso.obtenerEstadoMateria(materiaADejarDeCursar).trim();
-        assertThat("La materia deberia cambia su estado a PENDIENTE", estadoFinal, is("PENDIENTE"));
+        assertThat("La materia debería estar PENDIENTE", estadoFinal, is("PENDIENTE"));
 
         Boolean botonCursarDisponible = vistaProgreso.botonCursarEstaDisponible(materiaADejarDeCursar);
-        assertThat("El boton de cursar deberia esta disponible",  botonCursarDisponible, is(true));
+        assertThat("El botón de cursar debería estar disponible", botonCursarDisponible, is(true));
     }
 
-//    @Test
-//    public void queSePuedanFiltrarLasMateriasAprobadas(){
-//        realizarLoginComoTestUser();
-//        page.click("text=Ver progreso académico");
-//        page.waitForURL("**/progreso");
-//
-//        String materiaADejarDeCursar = "Taller Web II";
-//
-//        VistaProgreso vistaProgreso = new VistaProgreso(page);
-//
-//        Integer totalMaterias = vistaProgreso.obtenerMaterias().count();
-//
-//        vistaProgreso.filtrar("Aprobadas");
-//
-//        page.waitForURL("**/progreso?condicion=Aprobadas");
-//
-//        Integer totalMateriasAprobadas = vistaProgreso.obtenerMaterias().count();
-//        assertThat("Deberia haber al menos una materia aprobada", totalMateriasAprobadas, is(greaterThan(0)) );
-//
-//        Boolean todasLasMateriasAprobadas = vistaProgreso.todasLasMateriasVisibleTienenEstado("APROBADA");
-//        assertThat("Todas las materias mostradas deberian estar aprobadas", todasLasMateriasAprobadas, is(true));
-//
-//        vistaProgreso.filtrar(""); // El valor para "Todas" es un string vacío
-//
-//        // Espera a que la página se recargue
-//        page.waitForURL("**/progreso");
-//    }
+    // @Test
+    // public void queSePuedanFiltrarLasMateriasAprobadas() {
+    //     realizarLoginComoTestUser();
+    //     page.click("text=Ver progreso académico");
+    //     page.waitForURL("**/progreso");
+
+    //     VistaProgreso vistaProgreso = new VistaProgreso(page);
+    //     Integer totalMaterias = vistaProgreso.obtenerMaterias().count();
+
+    //     vistaProgreso.filtrar("Aprobadas");
+    //     page.waitForURL("**/progreso?condicion=Aprobadas");
+
+    //     Integer totalMateriasAprobadas = vistaProgreso.obtenerMaterias().count();
+    //     assertThat("Debería haber al menos una materia aprobada", totalMateriasAprobadas, is(greaterThan(0)));
+
+    //     Boolean todasLasMateriasAprobadas = vistaProgreso.todasLasMateriasVisibleTienenEstado("APROBADA");
+    //     assertThat("Todas las materias mostradas deberían estar aprobadas", todasLasMateriasAprobadas, is(true));
+
+    //     vistaProgreso.filtrar("");
+    //     page.waitForURL("**/progreso");
+    // }
 }
